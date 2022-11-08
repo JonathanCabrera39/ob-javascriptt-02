29a80469a33d11a9990df99648a5e60bae7e290e
Jonathan Gabriel Cabrera <subjona@gmail.com>
2022-11-08 14:44:34 -0300

    resuelto

---
+++ index.js
@@ -0,0 +1,33 @@
+let lista = [
+  "jonathan",
+  26,
+  true,
+  [ {
+    titulo: "game of thrones",
+    autor: "George R.R.Martin",
+    fecha: 1996,
+    url: "https//www.amazon.es/Juego-Tronos-Canci%C3%B3n-Hielo-Fuego/dp/8496208966",
+  }
+  ]
+]
+
+//lista.push(libros);
+
+let desarrolador = true;
+
+const fecha_nacimeinto = new Date(1996, 5, 3);
+
+if (desarrolador) {
+  console.log("soy desarrolador");
+} else {
+  console.log("no lo soy");
+}
+
+const libros = {
+  titulo: "game of thrones",
+  autor: "George R.R.Martin",
+  fecha: 1996,
+  url: "https//www.amazon.es/Juego-Tronos-Canci%C3%B3n-Hielo-Fuego/dp/8496208966",
+};
+
+console.log(lista);
